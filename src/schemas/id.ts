0a6f98d85bcf4159a911e85b7fa32a6efb7ccf93
satin <satin3737@gmail.com>
2025-12-31 14:05:52 +0200

    Added ObjectId validation to the id param schema

--- src/schemas/id.ts
+++ src/schemas/id.ts
@@ -1,6 +1,11 @@
+import mongoose from 'mongoose';
 import {z} from 'zod';
 
-const idSchema = z.string('Post ID must be a string').trim().min(1, 'Post ID is required');
+const idSchema = z
+    .string('Post ID must be a string')
+    .trim()
+    .min(1, 'Post ID is required')
+    .refine(id => mongoose.Types.ObjectId.isValid(id), {message: 'Id must be a 24 character hex string'});
 
 export const idParamsSchema = z.object({
     params: z.object({

388e8c2993cb06e8e9165a46fb0b55ac62669abf
satin <satin3737@gmail.com>
2025-12-31 13:36:37 +0200

    Updated migration template

--- src/database/migrations/1765719686901-change-image-field.ts
+++ src/database/migrations/1765719686901-change-image-field.ts
@@ -10,11 +10,11 @@ export async function up(): Promise<void> {
         const count = await Post.countDocuments(filter);
         if (!count) return;
         await Post.updateMany(filter, [{$set: {image: '$imageUrl'}}, {$unset: 'imageUrl'}], {updatePipeline: true});
-
-        await disconnectFromMongoServer();
     } catch (err) {
         Logger.error(err, 'Migration failed');
         throw err;
+    } finally {
+        await disconnectFromMongoServer();
     }
 }
 
@@ -26,10 +26,10 @@ export async function down(): Promise<void> {
         const count = await Post.countDocuments(filter);
         if (!count) return;
         await Post.updateMany(filter, [{$set: {imageUrl: '$image'}}, {$unset: 'image'}], {updatePipeline: true});
-
-        await disconnectFromMongoServer();
     } catch (err) {
         Logger.error(err, 'Rollback failed');
         throw err;
+    } finally {
+        await disconnectFromMongoServer();
     }
 }
--- src/database/template.ts
+++ src/database/template.ts
@@ -4,25 +4,23 @@ import {Logger} from '@/services';
 export async function up(): Promise<void> {
     try {
         await connectToMongoServer();
-
         // migration...
-
-        await disconnectFromMongoServer();
     } catch (err) {
         Logger.error(err, 'Migration failed');
         throw err;
+    } finally {
+        await disconnectFromMongoServer();
     }
 }
 
 export async function down(): Promise<void> {
     try {
         await connectToMongoServer();
-
         // rollback...
-
-        await disconnectFromMongoServer();
     } catch (err) {
         Logger.error(err, 'Rollback failed');
         throw err;
+    } finally {
+        await disconnectFromMongoServer();
     }
 }

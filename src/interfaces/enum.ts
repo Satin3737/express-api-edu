8a9d4eb32bf8f0dc14c71badb9919c39d192514c
satin <satin3737@gmail.com>
2025-12-31 13:36:59 +0200

    Added post image enum

--- src/interfaces/enum.ts
+++ src/interfaces/enum.ts
@@ -3,13 +3,16 @@ import type {IValueOf} from '@/interfaces';
 export const Routes = {
     posts: '/posts',
     post: '/post',
-    postById: '/post/:id'
+    postById: '/post/:id',
+    postImage: '/post-image',
+    postImageById: '/post-image/:id'
 } as const;
 
 export type IRoute = IValueOf<typeof Routes>;
 
 export const Models = {
-    post: 'Post'
+    post: 'Post',
+    postImage: 'PostImage'
 } as const;
 
 export type IModels = IValueOf<typeof Models>;

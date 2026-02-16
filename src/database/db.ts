5401efed628e7d3136ee0fbcea805ede81de5bbc
satin <satin3737@gmail.com>
2026-02-17 00:01:20 +0200

    Added mongo auth

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,13 +4,15 @@ import {Logger} from '@/services';
 const database = process.env.MONGO_INITDB_DATABASE;
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
+const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
 
-if (!database || !host || !port) {
+if (!database || !host || !port || !username || !password) {
     Logger.error('Missing MongoDB environment variables');
     process.exit(1);
 }
 
-export const MongoUrl = `mongodb://${host}:${port}/${database}`;
+export const MongoUrl = `mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin`;
 
 export const connectToMongoServer = async () => {
     try {
